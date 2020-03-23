ef1d210bba271c4422c00ea04bbad7f2e100b5b2
uxlinh <linh.tn94@gmail.com>
2020-03-23 14:39:42 +0100

    La inn oppgave 1e

--- oppgavesett-04/o-1/index.js
+++ oppgavesett-04/o-1/index.js
@@ -1,17 +1,41 @@
 
 /* a */
-let filmer = ["Batman Begins ", "Spirited Away ", "Parasite ", "The Truman Show "];
+let filmer = [" Batman Begins", " Spirited Away", " Parasite", " The Truman Show"];
 
 /* b */
-filmer = ["Mulan ", ...filmer]; 
+filmer = [" Mulan", ...filmer]; 
 
 /* c */
-filmer[3] = "Spartacus "
-document.write `
-Deloppgave (a, b, c): ${filmer}`
+filmer[3] = " Spartacus";
+opabc.innerHTML = `${filmer}`;
 
 /* d */
-listFilmer.innerHTML = "Deloppgave d";
+listFilmer.innerHTML = "";
 for(const film of filmer) {
     listFilmer.innerHTML += `<li>${film}</li>`;
 }
+
+/* e */
+const filmList = document.querySelector("#filmList");
+const inpFilm = document.querySelector("#inpFilm");
+const btn = document.querySelector("#btn");
+let movies = [];
+
+function visFilmer(){
+
+    filmList.innerHTML = "";
+
+    for(const mov of movies) {
+        filmList.innerHTML += `<li>${mov}</li>`;
+    }
+
+    inpFilm.value = ""; 
+    inpFilm.focus(); 
+}
+
+function leggTilFilm(){
+    movies = [inpFilm.value, ...movies]; //Hvis vi omrokkerer på denne så legger nye varen seg til sist på lista, og omvendt først på lista. 
+    visFilmer();
+}
+
+btn.onclick = leggTilFilm;
