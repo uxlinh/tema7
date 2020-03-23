c3c61ead527aab12b2b2f0bca701401848e9db3c
uxlinh <linh.tn94@gmail.com>
2020-03-23 13:10:29 +0100

    added oppgavesett 1

---
+++ oppgavesett-01/o-1/index.js
@@ -0,0 +1,12 @@
+let a = 2 + 3 * 2; 
+console.log(`a: 2 + 3 * 2 = ${a}`)
+let b = (2 + 3) * 2;
+console.log(`b: (2 + 3) * 2 = ${b}`)
+let c = 10 / 2 + 5; 
+console.log(`c: 10 / 2 + 5 = ${c}`)
+let d = 10 / 2 * 5; 
+console.log(`d: 10 / 2 * 5 = ${d}`)
+let e = 2**2;
+console.log(`e: 2**2 = ${e}`)  
+let f = 2**3; 
+console.log(`f: 2**3 = ${f}`) 
--- oppgavesett-01/o-1/oppg1_2-3/index.js
+++
@@ -1,6 +0,0 @@
-document.write( 2 + 3 * 2 ); 
-document.write( (2 + 3) * 2 );
-document.write( 10 / 2 + 5 ); 
-document.write( 10 / 2 *5 ); 
-document.write( 2**2 ); 
-document.write( 2**3 ); 
