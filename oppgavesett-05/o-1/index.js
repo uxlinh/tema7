fc906a4dfa56b889327d97ae0be01d61177262d1
uxlinh <linh.tn94@gmail.com>
2020-04-19 15:16:41 +0200

    added style oppgave 5

--- oppgavesett-05/o-1/index.js
+++ oppgavesett-05/o-1/index.js
@@ -6,10 +6,13 @@ let inpPassord2 = document.querySelector('#inpPassord2');
 function registrerPassord() {
     if (inpPassord1.value === inpPassord2.value) {
         feedback.innerHTML = `Passordene er like`;
+        feedback.style.color = "green";
     } else if (inpPassord1.value !== inpPassord2.value) {
         feedback.innerHTML = `Du må fylle inn begge passordene`;
+        feedback.style.color = "#e68b03";
     } else {
         feedback.innerHTML = `Passordene er ikke like`;
+        feedback.style.color = "red";
     }
 }
 //Du må fylle inn begge passordene
